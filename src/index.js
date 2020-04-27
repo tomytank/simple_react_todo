146682f3c9a64c4a6b9bbaa6bf7c7b39575ca666
tomytank <github@iamtom.com>
2020-04-27 18:53:26 +0200

    Basic code for Todo list creation and displaying

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import TodoList from "./TodoList";
+
 
 const destination = document.querySelector("#container")
 
 ReactDOM.render(
   <div>
-    <p>Hello</p>
+    <TodoList />
   </div>,
   destination
   
