77ebc87b9fe93d1528bc85ef63f148e53b07d872
tomytank <github@iamtom.com>
2020-04-29 10:52:39 +0200

    Adding persistant data support

---
+++ src/data.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        key: 1588148682930, 
+        state: "", 
+        text: "first"
+    },
+    {key: 1588148684665, state: "", text: "second"},
+    {key: 1588148711375, state: "", text: "third"},
+    {key: 1588148714479, state: "", text: "fourth"},
+    {key: 1588148716390, state: "", text: "fifth"}
+    
+]
