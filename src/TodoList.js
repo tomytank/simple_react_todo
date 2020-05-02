1f6933210004537902275948bd261c388280cbc6
tomytank <github@iamtom.com>
2020-05-02 12:27:58 +0200

    Program logic changes

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,6 +1,6 @@
 import React from "react";
 //import React, {Component} from "react";
-
+import data from "./data";
 //https://5ea938770ebc1d001649091c.mockapi.io/api/d2/
 import "./TodoList.css";
 
--- src/data.js
+++ src/data.js
@@ -7,6 +7,7 @@ export default [
     {key: 1588148684665, state: "", text: "second"},
     {key: 1588148711375, state: "", text: "third"},
     {key: 1588148714479, state: "", text: "fourth"},
-    {key: 1588148716390, state: "", text: "fifth"}
+    {key: 1588148716390, state: "", text: "fifth"},
+    {key: 1588148716390, state: "", text: "SIXTH"}
     
 ]
